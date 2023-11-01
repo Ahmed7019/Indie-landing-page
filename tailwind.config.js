1d5f8a72570bb1cb23c3b2cf6c0871fb558242bd
Ahmed7019 <ahmedabdallah7109@gmail.com>
2023-11-01 18:40:49 +0200

    added some designs of the mobile design
    Added font awesome to the assessts
    Added the font to the index file
    added layer with gradient class to input.css file
    styled the navbar , header and satrted working with the main file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
-      Fontfamily: 
+      fontFamily: 
       {
-        'dm':[ 'DM Sans',' sans-serif']
+        'dm':[ '"DM Sans"',' sans-serif']
+      },
+  
+      colors: {
+        'violet' : 'rgba(194,114,247,0.9058441558441559)',
+        'l-yellow' : 'rgba(231,178,103,1)',
+        'l-pink' : 'rgba(235,99,173,0.5698051948051948)'
       },
     },
   },
